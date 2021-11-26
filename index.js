e826282142d103672c69985c116642924d192f7a
Jack McNary <jack.mcnary@gmail.com>
2021-11-26 12:03:48 -0700

    adding employee role

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-console.log("this is a start");
+const mysql = require('mysql2');
+const consoleTable = require('console.table');
+const inquirer = require('inquirer');
+
+require('dotenv').config();
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    }
+);
+
+const openingPrompt
+
+const addDepartment
+
+addRole()
+// db promise query (select all from dept)
+
+const addEmployee = () => {
+    db.promise().query('SELECT * FROM roles')
+        .then(([rows]) => {
+            let roles = rows;
+            const roleChoices = roles.map(({ title, id }) => ({
+                name: title,
+                value: id
+            }));
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "managerName",
+                    message: "What is the employee's first name?"
+                },
+                {
+                    type: "input",
+                    name: "managerID",
+                    message: "What is the employee's last name?"
+                },
+                {
+                    type: "input",
+                    name: "managerEmail",
+                    message: "What is the manager's email?"
+                },
+                {
+                    type: "input",
+                    name: "officeNumber",
+                    message: "What is employee's role?",
+                    choices: roleChoices
+                },
+            ])
+                .then((data)) => {
+    let firstName = data.first;
+    let lastName = data.last;
+    let employeeRole = data.role;
+
+    db.promise().query('SELECT * FROM employee')
+}
